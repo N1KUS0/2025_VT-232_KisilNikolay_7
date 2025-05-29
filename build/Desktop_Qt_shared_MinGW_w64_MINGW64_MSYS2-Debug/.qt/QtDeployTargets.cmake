9c443d48e80e149fe7ec81418ec048302e66f340
N1KUS0 <kolakisl70@gmail.com>
2025-05-29 09:32:58 +0300

    smartptr

---
+++ build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_labs10_FILE C:/Users/User/Desktop/oop/labs10/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/labs10.exe)
+set(__QT_DEPLOY_TARGET_labs10_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_labs10_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
---
+++ smartptr.h
@@ -0,0 +1,36 @@
+#ifndef SMARTPTR_H
+#define SMARTPTR_H
+
+template <typename T>
+class SmartPtr {
+    T* obj;
+public:
+    explicit SmartPtr(T* obj = nullptr) : obj(obj) {}
+    ~SmartPtr() { delete obj; }
+
+    // Запрет копирования
+    SmartPtr(const SmartPtr&) = delete;
+    SmartPtr& operator=(const SmartPtr&) = delete;
+
+    // Поддержка перемещения
+    SmartPtr(SmartPtr&& other) noexcept : obj(other.obj) {
+        other.obj = nullptr;
+    }
+
+    SmartPtr& operator=(SmartPtr&& other) noexcept {
+        if (this != &other) {
+            delete obj;
+            obj = other.obj;
+            other.obj = nullptr;
+        }
+        return *this;
+    }
+
+    T* operator->() const { return obj; }
+    T& operator*() const { return *obj; }
+    T* get() const { return obj; }
+
+    explicit operator bool() const { return obj != nullptr; }
+};
+
+#endif // SMARTPTR_H
